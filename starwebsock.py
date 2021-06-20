43d7f51c92326be727684b7cd3453e17605a313d
s4b7r <github@s4b7r.de>
2021-06-20 15:08:12 +0200

    Extract index template from Python file

--- starwebsock.py
+++ starwebsock.py
@@ -11,106 +11,21 @@ import uvicorn
 from uuid import uuid4
 import asyncio
 import starlette
+from starlette.templating import Jinja2Templates
 
 from channels import Channel
 from clients import Client
 
 
-template = """\
-<!DOCTYPE HTML>
-<html>
-<head>
-    <script type = "text/javascript">
-
-
-        const createSocket = (channel_url) => {
-            const socket = new WebSocket(channel_url);
-            
-            socket.onopen = (event) => {
-                document.getElementById('status').textContent = `Connected`;
-            };
-            socket.onmessage = (event) => {
-                console.log(event.data);
-                document.getElementById('last_message').value = event.data;
-                var msg = JSON.parse(event.data);
-                if (msg.k === "your_client_id") {
-                    document.getElementById('your_client_id').value = msg.v;
-                } else if (msg.k ==="your_channel_id" ) {
-                    document.getElementById('channel_id').value = msg.v;
-                } else if (msg.k === "their_choice") {
-                    document.getElementById('their_choice').value = msg.v.their_choice;
-                } else if (msg.k === "channel_full") {
-                    document.getElementById('channel_id').value = "CHANNEL FULL";
-                    socket.close()
-                    document.getElementById('status').textContent = `Disconnected`;
-                }
-            };
-            socket.onclose = function() { 
-                console.log("Closing websocket channel");
-                document.getElementById('status').textContent = `Disconnected`;
-            };
-
-            window.addEventListener('unload', (event) => socket.close());
-
-            return socket;
-        }
-
-        const disconnectSocket = (socket) => {
-            socket.close();
-            document.getElementById('status').textContent = `Disconnected`;
-        };
-
-        const sendChoice = (socket) => {
-            var my_choice = document.getElementById('your_choice').value;
-            socket.send(JSON.stringify({'k': 'my_choice', 'v': my_choice}));
-        }
-
-        document.addEventListener("DOMContentLoaded", () => {
-            let socket;
-
-            const form = document.getElementById('connect_form');
-            form.addEventListener('submit', (event) => {
-                event.preventDefault();
-                if (socket) {
-                    disconnectSocket(socket);
-                }
-                var channel_id = String(document.getElementById('channel_id').value);
-                var client_id = String(document.getElementById('your_client_id').value);
-                socket = createSocket("ws://localhost:8000/ws/" + channel_id + "/" + client_id);;
-            });
-            const sendChoiceButton = document.getElementById('send_choice');
-            sendChoiceButton.addEventListener('click', () => sendChoice(socket));
-
-            const disconnectButton = document.getElementById('disconnect_button');
-            disconnectButton.addEventListener('click', () => disconnectSocket(socket));
-        });
-    </script>
-</head>
-<body>
-<h1>Status: <span id="status">Disconnected</span></h1>
-<form id="connect_form">
-    <input type="text" id="channel_id" placeholder="channel_id" style="width: 500px;" /><br>
-    <input type="text" id="your_client_id" placeholder="client id" style="width: 500px;" /><br>
-    <input type="text" id="your_choice" placeholder="your choice" style="width: 500px;" /><br>
-    <input type="text" id="their_choice" placeholder="<their choice>" readonly style="width: 500px;" /><br>
-    <input type="text" id="last_message" placeholder="<last message>" readonly style="width: 500px;" /><br>
-    <button type="submit">Connect</button>
-    <button type="button" id="disconnect_button">Disconnect</button>
-    <button type="button" id="send_choice">Send choice</button>
-</form>
-</body>
-</html>
-"""
-
-
 app = Starlette()
+templates = Jinja2Templates(directory='templates')
 
 channels = {}
 
 
 @app.route('/')
 async def homepage(request):
-    return HTMLResponse(Template(template).render())
+    return templates.TemplateResponse('index.html', {'request': request})
 
 
 def get_existing_or_new_channel(channel_id):
